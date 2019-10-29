d227c054824223da6842361b2d9d7a659e901e01
Eduardo Fong Dubon <eduardofongdubon@hotmail.com>
2019-10-29 12:29:24 -0600

    Creación de la interfaz repositorio

---
+++ Donaciones.Web/Data/Entities/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Donaciones.Web.Data.Entities
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+}
---
+++ Donaciones.Web/Data/Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace Donaciones.Web.Data.Repositories
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public interface IGenericRepository<T> where T : class
+    {
+        //método para obtner todos los datos
+        IQueryable<T> GetAll();
+        //método para obtener una entidad dado un Id
+        Task<T> GetByIdAsync(int id);
+        //método para crear una entidad
+        Task CreateAsync(T entity);
+        //método para actualizar una entidad
+        Task UpdateAsync(T entity);
+        //método para eliminar una entidad
+        Task DeleteAsync(T entity);
+        //método para verificar si existe una entidad dado un Id
+        Task<bool> ExistAsync(int id);
+    }
+}
