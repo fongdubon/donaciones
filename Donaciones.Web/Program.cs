5ddda5f2888a88a49de2c786c7bd89550e4a79e7
isf9864@umad.edu.mx <isf9864@umad.edu.mx>
2020-05-12 11:50:34 -0500

    Reubicacion de una linea

--- Donaciones.Web/Program.cs
+++ Donaciones.Web/Program.cs
@@ -18,7 +18,6 @@ namespace Donaciones.Web
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
     }
 }
